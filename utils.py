dd2696975678c5fc5da5344b3690c0978c6ca8a1
fteodoro803 <agustinteodoro803@gmail.com>
2025-10-20 16:53:02 +1100

    reset uses files from files.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+from files import DATABASE_FILE, GTFS_FILE, LAST_UPDATED_FILE, EXTRACTED_DIRECTORY
 
 def delete_file(path: str) -> None:
     """
@@ -21,8 +22,8 @@ def reset(enabled: bool) -> None:
 
     print(f"Resetting Files")
     print(f"Deleting extracted folder, last_updated.txt, gtfs.zip, gtfs.sqlite...")
-    delete_file("extracted")
-    delete_file("last_updated.txt")
-    delete_file("gtfs.zip")
-    delete_file("gtfs.sqlite")
+    delete_file(EXTRACTED_DIRECTORY)
+    delete_file(LAST_UPDATED_FILE)
+    delete_file(GTFS_FILE)
+    delete_file(DATABASE_FILE)
     print("Reset finished")
